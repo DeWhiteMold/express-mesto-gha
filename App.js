11e0277d0bc7dad4590f40b065820ac7a5f40f65
Nikolai Smirnov <dewhitemold@yandex.ru>
2023-03-22 15:32:02 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '641a302e89e9e5de6efc4912',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+
+app.use('/cards', require('./routes/cards'));
+
+app.listen(PORT, () => {
+  console.log('backend хуета')
+})
